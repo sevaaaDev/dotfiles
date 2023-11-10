05b8a2bccd5772e0fcad886ff401a16dc01ef25f
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-11-10 15:30:13 +0700

    add telescope fzf

--- dot_config/nvim/lua/plugin/telescope.lua
+++ dot_config/nvim/lua/plugin/telescope.lua
@@ -4,8 +4,11 @@ local M = {
 	event = "Bufenter",
 	cmd = { "Telescope" },
 	dependencies = {
-
 		{ "nvim-lua/plenary.nvim", lazy = true, commit = "50012918b2fc8357b87cff2a7f7f0446e47da174" },
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		},
 	},
 }
 
@@ -42,7 +45,17 @@ function M.config()
 				theme = "cursor",
 			},
 		},
+		extensions = {
+			fzf = {
+				fuzzy = true, -- false will only do exact matching
+				override_generic_sorter = true, -- override the generic sorter
+				override_file_sorter = true, -- override the file sorter
+				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				-- the default case_mode is "smart_case"
+			},
+		},
 	})
+	require("telescope").load_extension("fzf")
 end
 
 return M
