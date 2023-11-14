9f816bdf2c3e17876b965a4625bd7d30444ace33
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-11-14 12:34:26 +0700

    remove some env var

--- dot_config/zsh/plugin/export.zsh
+++ dot_config/zsh/plugin/export.zsh
@@ -9,9 +9,8 @@ export MANWIDTH=999
 export PATH="$HOME/.local/share/neovim/bin":$PATH
 export LESSHISTFILE="$XDG_STATE_HOME"/less/history
 export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
-export PYTHONUSERBASE="$XDG_DATA_HOME"/python
-export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME"/python
+# export PYTHONUSERBASE="$XDG_DATA_HOME"/python
+# export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME"/python
 export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
 export HISTFILE="$XDG_STATE_HOME"/bash/history
 
-eval "$(starship init zsh)"
