9893e55cda66c95aa8d37b1537428ae6ee5c1b75
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-11-15 17:13:34 +0700

    add cool telescope ui for code action

--- dot_config/nvim/lua/plugin/telescope.lua
+++ dot_config/nvim/lua/plugin/telescope.lua
@@ -9,6 +9,7 @@ local M = {
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 		},
+		{ "nvim-telescope/telescope-ui-select.nvim" },
 	},
 }
 
@@ -53,9 +54,25 @@ function M.config()
 				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
 				-- the default case_mode is "smart_case"
 			},
+			["ui-select"] = {
+				require("telescope.themes").get_dropdown({
+					-- specific_opts = {
+					--   [kind] = {
+					--     make_indexed = function(items) -> indexed_items, width,
+					--     make_displayer = function(widths) -> displayer
+					--     make_display = function(displayer) -> function(e)
+					--     make_ordinal = function(e) -> string
+					--   },
+					--   -- for example to disable the custom builtin "codeactions" display
+					--      do the following
+					--   codeactions = false,
+					-- }
+				}),
+			},
 		},
 	})
 	require("telescope").load_extension("fzf")
+	require("telescope").load_extension("ui-select")
 end
 
 return M
