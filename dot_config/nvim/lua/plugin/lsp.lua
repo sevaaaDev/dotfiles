940c33074e794233e501ce3d21a96d76026aecc6
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-11-18 14:46:09 +0700

    delete empty lines

--- dot_config/nvim/lua/plugin/lsp.lua
+++ dot_config/nvim/lua/plugin/lsp.lua
@@ -13,11 +13,9 @@ local M = {
 
 function M.config()
 	local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	capabilities.textDocument.completion.completionItem.snippetSupport = true
 	capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
-
 	local function lsp_keymaps(bufnr)
 		local function keymap(buf, mode, lhs, rhs, opts)
 			opts = opts or {}
