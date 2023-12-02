81495fef61b1ca020da7c8937bbf09d10f2b505f
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-12-02 12:36:32 +0700

    make custom statusline (what am i doing)

--- dot_config/nvim/lua/plugin/heirline.lua
+++ dot_config/nvim/lua/plugin/heirline.lua
@@ -1,94 +1,81 @@
 local M = {
 	"rebelot/heirline.nvim",
 	commit = "170e1b1fd7c11db00e46d802165fb277db601ae7",
+	event = "BufEnter",
 	dependencies = {
 		{ "nvim-tree/nvim-web-devicons", lazy = true, commit = "3af745113ea537f58c4b1573b64a429fefad9e07" },
 	},
-	enabled = false,
+	-- enabled = false,
 }
 function M.config()
 	local utils = require("heirline.utils")
 	local conditions = require("heirline.conditions")
 	local ViMode = {
-		-- get vim current mode, this information will be required by the provider
-		-- and the highlight functions, so we compute it only once per component
-		-- evaluation and store it as a component attribute
 		init = function(self)
 			self.mode = vim.fn.mode(1) -- :h mode()
 		end,
-		-- Now we define some dictionaries to map the output of mode() to the
-		-- corresponding string and color. We can put these into `static` to compute
-		-- them at initialisation time.
 		static = {
-			mode_names = { -- change the strings if you like it vvvvverbose!
-				n = "N",
-				no = "N?",
-				nov = "N?",
-				noV = "N?",
-				["no\22"] = "N?",
-				niI = "Ni",
-				niR = "Nr",
-				niV = "Nv",
-				nt = "Nt",
-				v = "V",
-				vs = "Vs",
-				V = "V_",
-				Vs = "Vs",
-				["\22"] = "^V",
-				["\22s"] = "^V",
-				s = "S",
-				S = "S_",
-				["\19"] = "^S",
-				i = "I",
-				ic = "Ic",
-				ix = "Ix",
-				R = "R",
-				Rc = "Rc",
-				Rx = "Rx",
-				Rv = "Rv",
-				Rvc = "Rv",
-				Rvx = "Rv",
-				c = "C",
-				cv = "Ex",
-				r = "...",
-				rm = "M",
-				["r?"] = "?",
-				["!"] = "!",
-				t = "T",
+			mode_names = {
+				["n"] = "NORMAL",
+				["no"] = "OP-PENDING",
+				["nov"] = "OP-PENDING",
+				["noV"] = "OP-PENDING",
+				["no\22"] = "OP-PENDING",
+				["niI"] = "NORMAL",
+				["niR"] = "NORMAL",
+				["niV"] = "NORMAL",
+				["nt"] = "NORMAL",
+				["ntT"] = "NORMAL",
+				["v"] = "VISUAL",
+				["vs"] = "VISUAL",
+				["V"] = "VISUAL",
+				["Vs"] = "VISUAL",
+				["\22"] = "VISUAL",
+				["\22s"] = "VISUAL",
+				["s"] = "SELECT",
+				["S"] = "SELECT",
+				["\19"] = "SELECT",
+				["i"] = "INSERT",
+				["ic"] = "INSERT",
+				["ix"] = "INSERT",
+				["R"] = "REPLACE",
+				["Rc"] = "REPLACE",
+				["Rx"] = "REPLACE",
+				["Rv"] = "VIRT REPLACE",
+				["Rvc"] = "VIRT REPLACE",
+				["Rvx"] = "VIRT REPLACE",
+				["c"] = "COMMAND",
+				["cv"] = "VIM EX",
+				["ce"] = "EX",
+				["r"] = "PROMPT",
+				["rm"] = "MORE",
+				["r?"] = "CONFIRM",
+				["!"] = "SHELL",
+				["t"] = "TERMINAL",
 			},
 			mode_colors = {
-				n = "red",
-				i = "green",
-				v = "cyan",
-				V = "cyan",
+				n = "#f38ba8",
+				i = "cyan",
+				v = "orange",
+				V = "orange",
 				["\22"] = "cyan",
-				c = "orange",
+				c = "white",
 				s = "purple",
 				S = "purple",
 				["\19"] = "purple",
 				R = "orange",
 				r = "orange",
-				["!"] = "red",
-				t = "red",
+				["!"] = "brown",
+				t = "brown",
 			},
 		},
-		-- We can now access the value of mode() that, by now, would have been
-		-- computed by `init()` and use it to index our strings dictionary.
-		-- note how `static` fields become just regular attributes once the
-		-- component is instantiated.
-		-- To be extra meticulous, we can also add some vim statusline syntax to
-		-- control the padding and make sure our string is always at least 2
-		-- characters long. Plus a nice Icon.
 		provider = function(self)
-			return " %2(" .. self.mode_names[self.mode] .. "%)"
+			return " %2(" .. self.mode_names[self.mode] .. "%) "
 		end,
-		-- Same goes for the highlight. Now the foreground will change according to the current mode.
 		hl = function(self)
 			local mode = self.mode:sub(1, 1) -- get only the first mode character
-			return { fg = self.mode_colors[mode], bold = true }
+			return { fg = "black", bg = self.mode_colors[mode], bold = true }
 		end,
-		-- Re-evaluate the component only on ModeChanged event!
-		-- Also allows the statusline to be re-evaluated when entering operator-pending mode
 		update = {
 			"ModeChanged",
 			pattern = "*:*",
@@ -97,94 +84,144 @@ function M.config()
 			end),
 		},
 	}
-
-	local FileNameBlock = {
-		-- let's first set up some attributes needed by this component and it's children
+	local Git = {
+		condition = conditions.is_git_repo,
 		init = function(self)
-			self.filename = vim.api.nvim_buf_get_name(0)
+			self.dict = vim.b.gitsigns_status_dict
+		end,
+		hl = {
+			fg = "#fab387",
+		},
+		provider = function(self)
+			return "  " .. self.dict.head .. " "
 		end,
 	}
-	-- We can now define some children separately and add them later
+	local Filename = {
+		provider = " %t",
+	}
+	local Divider = {
+		provider = "%=",
+	}
+	local Fileicon = {
+		provider = function()
+			local filename = vim.api.nvim_buf_get_name(0)
+			local extension = vim.fn.fnamemodify(filename, ":t:e")
+			local icon =
+				require("nvim-web-devicons").get_icon(vim.fn.fnamemodify(filename, ":t"), extension, { default = true })
+			return icon
+		end,
+	}
+	local Modified = {
+		provider = "%m",
+	}
+	local Percentage = {
+		provider = "%p%%",
+	}
+	local LSPinfo = {
+		condition = conditions.lsp_attached,
+		provider = function()
+			local clients = vim.lsp.get_active_clients()
+			local clients_list = {}
+			for _, client in pairs(clients) do
+				table.insert(clients_list, client.name)
+			end
+			local name = table.concat(clients_list, ", ")
+			return " [" .. name .. "] "
+		end,
+		hl = { fg = "#cba6f7" },
+	}
 
-	local FileIcon = {
-		init = function(self)
-			local filename = self.filename
-			local extension = vim.fn.fnamemodify(filename, ":e")
-			self.icon, self.icon_color =
-				require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
+	local Formatters = {
+		condition = function(self)
+			local next = next
+			self.conform = require("conform")
+			if next(self.conform.list_formatters()) == nil then
+				return false
+			end
+			return true
 		end,
 		provider = function(self)
-			return self.icon and (self.icon .. " ")
+			local fmts = self.conform.list_formatters()
+			local fmt_list = {}
+			for _, fmt in pairs(fmts) do
+				table.insert(fmt_list, fmt.name)
+			end
+			local name = table.concat(fmt_list, ", ")
+			return "󰛗 [" .. name .. "] "
 		end,
-		hl = function(self)
-			return { fg = self.icon_color }
-		end,
+		hl = { fg = "#a6e3a1" },
 	}
 
-	local FileName = {
-		provider = function(self)
-			-- first, trim the pattern relative to the current directory. For other
-			-- options, see :h filename-modifers
-			local filename = vim.fn.fnamemodify(self.filename, ":.")
-			if filename == "" then
-				return "[No Name]"
-			end
-			-- now, if the filename would occupy more than 1/4th of the available
-			-- space, we trim the file path to its initials
-			-- See Flexible Components section below for dynamic truncation
-			if not conditions.width_percent_below(#filename, 0.25) then
-				filename = vim.fn.pathshorten(filename)
-			end
-			return filename
-		end,
-		hl = { fg = utils.get_highlight("Directory").fg },
-	}
+	local Diagnostics = {
 
-	local FileFlags = {
+		condition = conditions.has_diagnostics,
+
+		static = {
+			error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+			warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+			info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+			hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+		},
+
+		init = function(self)
+			self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+			self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+			self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+			self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+		end,
+
+		update = { "DiagnosticChanged", "BufEnter" },
+
 		{
-			condition = function()
-				return vim.bo.modified
+			provider = function(self)
+				-- 0 is just another output, we can decide to print it or not!
+				return self.errors > 0 and (self.error_icon .. self.errors .. " ")
 			end,
-			provider = "[+]",
-			hl = { fg = "green" },
+			hl = { fg = "#f38ba8" },
 		},
 		{
-			condition = function()
-				return not vim.bo.modifiable or vim.bo.readonly
+			provider = function(self)
+				return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
 			end,
-			provider = "",
-			hl = { fg = "orange" },
+			hl = { fg = "#f9e2af" },
+		},
+		{
+			provider = function(self)
+				return self.info > 0 and (self.info_icon .. self.info .. " ")
+			end,
+			hl = { fg = "#89dceb" },
+		},
+		{
+			provider = function(self)
+				return self.hints > 0 and (self.hint_icon .. self.hints)
+			end,
+			hl = { fg = "#89dceb" },
 		},
 	}
-
-	-- Now, let's say that we want the filename color to change if the buffer is
-	-- modified. Of course, we could do that directly using the FileName.hl field,
-	-- but we'll see how easy it is to alter existing components using a "modifier"
-	-- component
-
-	local FileNameModifer = {
-		hl = function()
-			if vim.bo.modified then
-				-- use `force` because we need to override the child's hl foreground
-				return { fg = "cyan", bold = true, force = true }
-			end
+	local buf = {
+		provider = function()
+			local num = vim.fn.len(vim.fn.getbufinfo({ buflisted = 1 }))
+			return "BufOpn : " .. num
 		end,
 	}
-
-	-- let's add the children to our FileNameBlock component
-	FileNameBlock = utils.insert(
-		FileNameBlock,
-		FileIcon,
-		utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
-		FileFlags,
-		{ provider = "%<" } -- this means that the statusline is cut here when there's not enough space
-	)
 	local status = {
 		ViMode,
-		FileNameBlock,
+		Git,
+		Diagnostics,
+		Divider,
+		{ Fileicon, Filename, Modified },
+		Divider,
+		Formatters,
+		LSPinfo,
+		Percentage,
+	}
+	local tabline = {
+		Divider,
+		buf,
 	}
 	require("heirline").setup({
 		statusline = status,
+		tabline = tabline,
 	})
 end
 
